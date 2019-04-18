ac6bc6bd5842192ae4fc49e8e365911ba65ae0fc
Comfy321 <lonelypyxel@yandex.com>
2019-04-17 21:20:50 -0700

    stuff

--- PlayMenu.java
+++ PlayMenu.java
@@ -1,11 +1,33 @@
 import java.util.*;
+import java.io.*;
 class PlayMenu implements Menu
 {
+	public static ArrayList<String> prompts = new ArrayList<String>();
 	public static void genPrompts()
 	{
+		try
+		{
+			BufferedReader reader = new BufferedReader(new FileReader("promptList"));
+			String line = reader.readLine();
+			while(line != null)
+			{
+				prompts.add(line);
+				line = reader.readLine();
+			}
+		}
+		catch(Exception e)
+		{ System.out.println(e); }
 	}
 	public static void Display()
 	{
-		System.out.println("Play Menu!");
+		genPrompts();
+		System.out.println("---Play---\n0) Back");
+		for(int i=1; i<=prompts.size(); i++)
+			System.out.println(i +") " +prompts.get(i-1));
+		Scanner scan = new Scanner(system.in);
+		while(true)
+		{
+			
+		}
 	}
 }
