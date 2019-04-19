3da568b136ec74457a6043b19d2dbbafddca94a1
Comfy321 <lonelypyxel@yandex.com>
2019-04-18 18:13:08 -0700

    stuff

--- PlayMenu.java
+++ PlayMenu.java
@@ -24,10 +24,30 @@ class PlayMenu implements Menu
 		System.out.println("---Play---\n0) Back");
 		for(int i=1; i<=prompts.size(); i++)
 			System.out.println(i +") " +prompts.get(i-1));
-		Scanner scan = new Scanner(system.in);
+		Scanner s = new Scanner(System.in);
+		int choice =-1;
 		while(true)
 		{
-			
+			try{choice=s.nextInt();}
+			catch(Exception e) { choice=-1; }
+			if(choice>=0 && choice < prompts.size()+1)
+					break;
 		}
+		choice--;
+		System.out.println(prompts.get(choice));
 	}
+	public static double Play(String s) //returns time it take to type prompt.
+	{
+		MainMenu.Clear();
+		Scanner s = new Scanner(System.in);
+		System.out.println(s);
+		long starttime=System.nanoTime();
+		String input=s.nextLine();
+		long endtime=System.nanoTime();
+		long time=(starttime-endtime)/1000; //time is in seconds
+
+	}
+	public static double Score(string i; long t) //calculate score based on time and similarity
+	{
+       	}
 }
