2e67b07cea65c9fd4bb0f67d3f77a2ae50410c01
Jacob Woods (LonelyPyxel) <lonelypyxel@yandex.com>
2019-04-26 12:29:00 -0700

    Buffffffff

--- StatMenu.java
+++ StatMenu.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.io.*;
 public class StatMenu implements Menu
 {
 	public static void Display()
@@ -22,7 +24,20 @@ public class StatMenu implements Menu
 				choice--;
 			else
 				MainMenu.Display();
-
+			int lineno = new MainMenu(0,0,0,prompt).getLineNumberofPrompt();
+			BufferedReader reader;
+			try {
+				reader = new BufferedReader(new FileReader("stats"));
+				String line = reader.readLine();
+				while (line != null)
+			       	{
+					System.out.println(line);
+					// read next line
+					line = reader.readLine();
+				}
+				reader.close();
+			} 
+			catch(IOException e) {System.out.Println(e)};
 		}
 	}
 }
